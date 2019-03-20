f57f156827d7c0ddab47f38cc7f101ebf90043e9
jonathan nshuti <nshutijonathan130@gmail.com>
2019-03-20 10:51:25 -0700

    [ft-todo]added comments.

--- class_todo.js
+++ class_todo.js
@@ -1,21 +1,23 @@
+
 //i worked together with mirelle in the bootcamp!
+//class definition
 class jobsTodo{
    constructor(id,jobTitle,jobdate,done){
        this.jobTitle = jobTitle;
        this.jobdate = jobdate;
        this.done = false;
    }
-
+//function definition within parent class 
    display(){
        return `This job of ${this.jobTitle} has been created on ${this.jobdate} is ${this.done}`
 
    }
-
+//function definition within parent class 
    donejob(){
        this.done = true;
    }
 }
-
+//inheritance
 class subjob extends jobsTodo{
    constructor(id,jobTitle,jobdate,done) {
        super(id,jobTitle,jobdate,done);
@@ -24,7 +26,7 @@ class subjob extends jobsTodo{
 }
 
 let job= [];
-let newjob = new subjob(1,'selecting Fellows','19/03/2019');
+let newjob = new subjob(1,'selecting Fellows','19/03/2019');//creating instances
 job.push(newjob);
 console.log(job[0].display());
 job[0].donejob();
